ec28079e52fe34b691b869f2ecfff358d106a349
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-09-08 23:47:56 +0700

    Import fonts

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const LandingPage: React.FC = () => {
-    return <div className='p-16'>Landing Page</div>;
+    return (
+        <div className='p-16'>
+            Landing Page
+        </div>
+    );
 };
 
 export default LandingPage
