93366aaa31a9e2d77c5e8c237c84625a17343de1
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-09-08 23:37:22 +0700

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import './index.css';
+
+import LandingPage from './pages/landing';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
---
+++ src/pages/landing.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+    return <div className='p-16'>Landing Page</div>;
+};
+
+export default LandingPage
